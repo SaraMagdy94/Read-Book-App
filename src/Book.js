d39e430e65b107bae355dfb6b8f526126819cba9
SaraMagdy94 <30289008+SaraMagdy94@users.noreply.github.com>
2018-07-19 18:19:01 +0200

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -27,7 +27,7 @@ class Book extends React.Component {
             <select 
              value={this.props.book.shelf || "none"}
                             onChange={(event) => this.handelChange(event.target.value) }>
-                            <option value="none" disabled>Move to...</option>
+                            <option  disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
                             <option value="read">Read</option>
