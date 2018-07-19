9953b0c6b81af08faa15a3b205298fb771f80210
SaraMagdy94 <30289008+SaraMagdy94@users.noreply.github.com>
2018-07-19 08:03:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import HomePage from './HomePage';
 import Search from './Search';
 import * as BooksAPI from './BooksAPI'
 import './App.css'
-
 class BooksApp extends React.Component {
   state =
    {
@@ -13,32 +12,44 @@ class BooksApp extends React.Component {
 
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
-      this.setState({ books })
-    })
-  }
-  changeShelf = (book, shelf) => {
-    BooksAPI.update(book, shelf);
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books: books })
-    })
+      this.setState({ books: books });
+    });
   }
 
+  hanelShelfChange = (book, shelf) => {
+    BooksAPI.update(book, shelf)
+    .then(() => {
+      book.shelf=shelf;
+      this.setState(state =>({
+        books:state.books.filter(item=> item.id !== book.id).concat([book])
+     }))
+    })
+  }
+  
+ 
   render() {
+
     const { books } = this.state;
+    if(books.length === 0){
+      return null
+    }
 
     return (
   
       <div className="app">
-        <Route exact path='/' render={() => (
+        <Route exact path='/' render={({history}) => (
           <HomePage
               books={ books }
-              changeShelf={this.changeShelf}
+             sendShelfChange={(book, shelf) => { this.hanelShelfChange(book, shelf) }}
             />
          )}
         />
           
-        <Route path='/search' render={() => (
-          <Search />
+        <Route path='/search' render={({history}) => (
+          <Search
+            sendShelfChange={(book, shelf) => { this.hanelShelfChange(book, shelf) }}
+            shelfBooks={this.state.books}
+           />
           )}
         />
       </div>
