9ba0d1b7b12a53e4ae5d840d9524aba60eeee27b
SaraMagdy94 <30289008+SaraMagdy94@users.noreply.github.com>
2018-07-19 08:14:50 +0200

    Update Shelf.js

--- src/Shelf.js
+++ src/Shelf.js
@@ -2,23 +2,32 @@ import React from 'react';
 import Book from './Book';
 
 class Shelf extends React.Component {
+
+    sendShelfChange(book,shelf){
+        this.props.sendShelfChange(book, shelf)
+    }
+    
     render(){
-        const { books,changeShelf } = this.props
+        const { books } = this.props
+        if (this.props.books.length === 0) {
+            return null;
+        }
         return(
             <div className="bookshelf">
-            <h2 className="bookshelf-title">{ this.props.name }</h2>
+                <h2 className="bookshelf-title">{this.props.title }</h2>
             <div className="bookshelf-books">
             <ol className="books-grid">
                {
-                   books.map((book) => (
-						<li key={book.id}>
+                  books.length > 0 &&
+                   books.map((item) => (
+						<li key={item.id}>
 							<Book
-                                book={book}
-                                authors={ book.authors }
-                                title={ book.title }
-                                imageLinks={ book.imageLinks }
-                                shelf={ book.shelf }
-                                changeShelf={ changeShelf }
+                                book={item}
+                                authors={ item.authors }
+                                title={ item.title }
+                                imageLinks={ item.imageLinks }
+                                sendShelfChange={(book, shelf) => { this.sendShelfChange(book, shelf)}}
+                              
 							/>
 						</li>
                    ))
